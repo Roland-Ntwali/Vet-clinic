b994007943ba4ed0d55916d39634bbfd123b3e69
Roland-Ntwali <rolandntwali@gmail.com>
2023-06-08 12:25:10 +0200

    Modified the table

--- schema.sql
+++ schema.sql
@@ -22,3 +22,28 @@ CREATE TABLE species (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100)
 );
+
+-- Remove the existing animals table if it already exists (optional)
+DROP TABLE IF EXISTS animals;
+
+-- Create the modified animals table
+CREATE TABLE animals (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL,
+    species_id INT REFERENCES species(id),
+    owner_id INT REFERENCES owners(id)
+);
+
+-- Add a foreign key constraint for species_id column
+ALTER TABLE animals ADD CONSTRAINT fk_species_id
+FOREIGN KEY (species_id)
+REFERENCES species(id);
+
+-- Add a foreign key constraint for owner_id column
+ALTER TABLE animals ADD CONSTRAINT fk_owner_id
+FOREIGN KEY (owner_id)
+REFERENCES owners(id);
