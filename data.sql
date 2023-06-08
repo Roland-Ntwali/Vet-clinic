c3d2c1f90106bcef23688f4ddd5543e8d55362f2
Roland-Ntwali <rolandntwali@gmail.com>
2023-06-08 12:27:45 +0200

    Add data to owners table

--- data.sql
+++ data.sql
@@ -10,3 +10,13 @@ VALUES ('Agumon', '2020-02-03', 0, true, 10.23),
        ('Boarmon', '2005-06-07', 7, true, 20.4),
        ('Blossom', '1998-10-13', 3, true, 17),
        ('Ditto', '2022-05-14', 4, true, 22);
+
+-- Insert data into the owners table
+INSERT INTO owners (full_name, age) VALUES
+    ('Sam Smith', 34),            -- Sam Smith, 34 years old
+    ('Jennifer Orwell', 19),      -- Jennifer Orwell, 19 years old
+    ('Bob', 45),                  -- Bob, 45 years old
+    ('Melody Pond', 77),          -- Melody Pond, 77 years old
+    ('Dean Winchester', 14),      -- Dean Winchester, 14 years old
+    ('Jodie Whittaker', 38);      -- Jodie Whittaker, 38 years old
+    
